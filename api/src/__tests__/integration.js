c198ecff12c48b4b3af1d7452f713c27124a9727
Rayan Saidada <Rayan.saidada@student.ehb.be>
2021-12-24 05:31:09 +0100

    Testing: genre CRUD

--- api/src/__tests__/integration.js
+++ api/src/__tests__/integration.js
@@ -3,73 +3,146 @@ const server = require('../server.js');
 const request = supertest(server);
 
 
-describe('POST /create', () => {
+
+
+describe('POST /createGame', () => {
   test('should responds with code 200', () => {
     try {
-      request.get('/create').expect(200);
+      request.get('/createGame').expect(200);
     } catch (err) {
     }
   });
 });
 
 
-describe('GET /getAll', () => {
-    test('should give an array',  () => {
+describe('POST /createGenre', () => {
+  test('should responds with code 200', () => {
+    try {
+      request.get('/createGenre').expect(200);
+    } catch (err) {
+    }
+  });
+});
+
+
+describe('GET /getAllGames', () => {
+    test('should give all games',  () => {
       try {
-         request.get('/getAll').expect(Object);
+         request.get('/getAllGames').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('GET /getByID', () => {
-    test('should give an array',  () => {
+  describe('GET /getAllGenres', () => {
+    test('should give all genres',  () => {
       try {
-         request.get('/getByID').expect(Object);
+         request.get('/getAllGenres').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('GET /getByName', () => {
-    test('should give an array',  () => {
+
+
+
+describe('GET /getGamesByID/:id', () => {
+    test('should give a game by given id',  () => {
       try {
-         request.get('/getByName').expect(Object);
+         request.get('/getGamesByID').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('GET /getByPrice', () => {
-    test('should give an array',  () => {
+  describe('GET /getGenreByID/:id', () => {
+    test('should give a genre by given id',  () => {
       try {
-         request.get('/getByPrice').expect(Object);
+         request.get('/getGenreByID').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('GET /getByDeveloper', () => {
-    test('should give an array',  () => {
+
+
+describe('GET /getGamesByName/:name', () => {
+    test('should give a game by given name',  () => {
       try {
-         request.put('/getByDeveloper').expect(Object);
+         request.get('/getGamesByName').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('PUT /updateByID', () => {
-    test('should give an array',  () => {
+
+  describe('GET /getGenreByGenre/:genre', () => {
+    test('should give a genre by given genre',  () => {
       try {
-         request.put('/updateByID').expect(Object);
+         request.get('/getGenreByGenre').expect(Object);
       } catch (err) {
       }
     });
   });
 
-describe('DEL /deleteByID', () => {
-    test('should give an array',  () => {
-        request.del('/deleteByID').expect(200);
+
+describe('GET /getGamesByPrice/:price', () => {
+    test('should give a game by given price',  () => {
+      try {
+         request.get('/getGamesByPrice').expect(Object);
+      } catch (err) {
+      }
     });
   });
 
+describe('GET /getGamesByDeveloper/:developer', () => {
+    test('should give a game by given developer',  () => {
+      try {
+         request.put('/getGamesByDeveloper').expect(Object);
+      } catch (err) {
+      }
+    });
+  });
+
+
+  describe('GET /getGamesByGenre/:genre', () => {
+    test('should give a game by given genre',  () => {
+      try {
+         request.put('/getGamesByGenre').expect(Object);
+      } catch (err) {
+      }
+    });
+  });
+
+
+describe('PUT /updateGamesByID/:id', () => {
+    test('should responds with code 200',  () => {
+      try {
+         request.put('/updateByID').expect(200);
+      } catch (err) {
+      }
+    });
+  });
+
+  describe('PUT /updateGenresByID/:id', () => {
+    test('should responds with code 200',  () => {
+      try {
+         request.put('/updateGenresByID').expect(200);
+      } catch (err) {
+      }
+    });
+  });
+
+describe('DEL /deleteGamesByID/:id', () => {
+    test('should delete a game by given id',  () => {
+        request.del('/deleteGamesByID').expect(200);
+    });
+  });
+
+
+
+  describe('DEL /deleteGenreByID/:id', () => {
+    test('should delete a genre by given id',  () => {
+        request.del('/deleteGenreByID').expect(200);
+    });
+  });
